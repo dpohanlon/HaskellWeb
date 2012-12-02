2ed24f8f57a03346e0bec92d5c391ca2de321b02
Daniel O'Hanlon <dpohanlon@gmail.com>
2012-12-02 13:53:51 +0000

    Fixed formatting.

--- HTTPRequestParser.hs
+++ HTTPRequestParser.hs
@@ -1,59 +1,59 @@
 module HTTPRequestParser
 (
-	HTTPRequest(..),
-	parseHTTP
+    HTTPRequest(..),
+    parseHTTP
 ) where
 
 import Text.ParserCombinators.Parsec
 
 data HTTPRequest = HTTPRequest {
-	reqSite :: String,
-	reqURL :: String
+    reqSite :: String,
+    reqURL :: String
 } deriving (Eq, Show)
 
 httpReq = endBy line eol
 
 line = getURL
-	<|> getHost
-	<?> "Request Header"
+    <|> getHost
+    <?> "Request Header"
 
 getURL = do
-	string "GET "
-	url <- many $ noneOf ",\n\r "
-	string " HTTP/"
-	httpVer <- many $ noneOf ",\n\r"
-	return url
+    string "GET "
+    url <- many $ noneOf ",\n\r "
+    string " HTTP/"
+    httpVer <- many $ noneOf ",\n\r"
+    return url
 
 getHost = do
-	string "Host: "
-	h <- many $ noneOf ",\n\r"
-	return h
+    string "Host: "
+    h <- many $ noneOf ",\n\r"
+    return h
 
 eol = try (string "\n\r")
-	<|> try (string "\r\n")
-	<|> string "\n"
-	<|> string "\r"
-	<?> "EOL"
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+    <?> "EOL"
 
 --eol = string "\n\r"
 
 httpInfo :: [String] -> HTTPRequest
 httpInfo info = HTTPRequest{ reqSite = info !! 1,
-							 reqURL =  info !! 0 }
+                             reqURL =  info !! 0 }
 
 --parseHTTP :: String -> Maybe HTTPRequest
 --parseHTTP input = do
---	case parse httpReq "HaskellWeb" input of
---		Left _ -> Nothing
---		Right r -> Just $ httpInfo r
+--  case parse httpReq "HaskellWeb" input of
+--      Left _ -> Nothing
+--      Right r -> Just $ httpInfo r
 
 parseHTTP :: String -> HTTPRequest
 parseHTTP input = do
-	case parse httpReq "HaskellWeb" input of
-		Right r -> httpInfo r
-		Left _ -> httpInfo [" "," "] -- Please fix me!
+    case parse httpReq "HaskellWeb" input of
+        Right r -> httpInfo r
+        Left _ -> httpInfo [" "," "] -- Please fix me!
 
 main = do
-	c <- getContents
-	print $ parseHTTP c
-	
+    c <- getContents
+    print $ parseHTTP c
+    
--- ws.hs
+++ ws.hs
@@ -8,10 +8,10 @@ Logs in /var/www/logs/server.log as TIME/DATE HOST REQUEST
 Fully daemonized - control with {start, stop, restart}
 
 TODO:
-	Configuration file - set log settings and file paths
-	Respond to more HTTP requests (HEAD, etc)
-	Syntax
-	Server statistics
+    Configuration file - set log settings and file paths
+    Respond to more HTTP requests (HEAD, etc)
+    Syntax
+    Server statistics
 
 -}
 
@@ -22,7 +22,7 @@ import System.Environment (getArgs)
 import System.FilePath.Posix (takeExtension)
 import System.Posix.Files (fileExist)
 import System.IO (hSetBuffering, hGetLine, hGetContents, hPutStr, hPutStrLn, hFileSize, openFile,
-				  IOMode(ReadMode), FilePath, BufferMode(..), Handle)
+                  IOMode(ReadMode), FilePath, BufferMode(..), Handle)
 import Control.Concurrent (forkIO)
 import Text.Regex.Posix
 import Data.Time.LocalTime
@@ -37,7 +37,7 @@ main :: IO ()
 main = serviced runHaskellWeb
 
 runHaskellWeb :: CreateDaemon Socket -- privilegedAction (IO a) -> CreateDaemon (a)
-runHaskellWeb = simpleDaemon { program          = haskellWeb, 
+runHaskellWeb = simpleDaemon { program          = haskellWeb,
                                privilegedAction = bindPort }
 
 bindPort :: IO Socket
@@ -45,78 +45,78 @@ bindPort = withSocketsDo . listenOn . PortNumber . fromIntegral . read $ "80"
 
 haskellWeb :: Socket -> IO ()
 haskellWeb socket = withSocketsDo $ do
-	syslog Notice "HaskellWeb running"
-	sockHandler socket
+    syslog Notice "HaskellWeb running"
+    sockHandler socket
 
 sockHandler :: Socket -> IO ()
 sockHandler socket = do
-	(handle, host, _) <- accept socket
-	hSetBuffering handle NoBuffering
-	forkIO $ requestHandler handle host
-	sockHandler socket
+    (handle, host, _) <- accept socket
+    hSetBuffering handle NoBuffering
+    forkIO $ requestHandler handle host
+    sockHandler socket
 
 requestHandler :: Handle -> String -> IO ()
 requestHandler handle host = do
-	request <- hGetContents handle
-	--logRequest request host
-	requestParser handle request
+    request <- hGetContents handle
+    --logRequest request host
+    requestParser handle request
 
 logRequest :: String -> String -> IO ()
 logRequest request host = do
-	pT <- getPOSIXTime
-	let t = posixSecondsToUTCTime pT
-	let log =  show t ++ "\t" ++ host ++ "\t" ++ request ++ "\n"
-	appendFile "/var/www/logs/server.log" log
+    pT <- getPOSIXTime
+    let t = posixSecondsToUTCTime pT
+    let log =  show t ++ "\t" ++ host ++ "\t" ++ request ++ "\n"
+    appendFile "/var/www/logs/server.log" log
 
 -- Parses HTTP request of the form "GET /foo/bar/baz.html HTTP/1.1\r"
 requestParser :: Handle -> String -> IO ()
 requestParser handle request = do
-	let httpReq = parseHTTP request
-	let path = reqURL httpReq
-	let site = stripWWW $ reqSite httpReq
-	if null (stripFS path) then
-		servePage handle site "/index.html"
-	else
-		servePage handle site path
+    let httpReq = parseHTTP request
+    let path = reqURL httpReq
+    let site = stripWWW $ reqSite httpReq
+    if null (stripFS path) then
+        servePage handle site "/index.html"
+    else
+        servePage handle site path
 
 servePage :: Handle -> String -> String -> IO ()
 servePage handle site path = do
-	let fullPath = "/var/www/" ++ site ++ path
-	let notFoundPath = "/var/www/" ++ site ++ "/404.html"
-	pathExists <- fileExist fullPath
-	if pathExists
-		then do -- hook here?
-			serveHeader handle fullPath "200 OK"
-			serveFile handle fullPath
-		else do
-			serveHeader handle notFoundPath "404 Not Found"
-			serveFile handle notFoundPath
+    let fullPath = "/var/www/" ++ site ++ path
+    let notFoundPath = "/var/www/" ++ site ++ "/404.html"
+    pathExists <- fileExist fullPath
+    if pathExists
+        then do -- hook here?
+            serveHeader handle fullPath "200 OK"
+            serveFile handle fullPath
+        else do
+            serveHeader handle notFoundPath "404 Not Found"
+            serveFile handle notFoundPath
 
 serveFile :: Handle -> String -> IO ()
 serveFile handle path = do
-	fileContents <- ByteS.readFile path
-	ByteS.hPutStr handle fileContents
-	hClose handle -- sClose handle ??
+    fileContents <- ByteS.readFile path
+    ByteS.hPutStr handle fileContents
+    hClose handle -- sClose handle ??
 
 serveHeader :: Handle -> String -> String -> IO ()
 serveHeader handle path status = do
-	fileSize <- getFileSize path
-	let contentType = getContentType path
-	hPutStr handle $ "HTTP/1.1 " ++ status ++ "\r\n"
-	hPutStr handle $ "Content-Type: " ++ contentType ++ "\r\n"
-	hPutStr handle $ "Content-Length: " ++ show fileSize ++ "\r\n"
-	hPutStr handle "\r\n"
+    fileSize <- getFileSize path
+    let contentType = getContentType path
+    hPutStr handle $ "HTTP/1.1 " ++ status ++ "\r\n"
+    hPutStr handle $ "Content-Type: " ++ contentType ++ "\r\n"
+    hPutStr handle $ "Content-Length: " ++ show fileSize ++ "\r\n"
+    hPutStr handle "\r\n"
 
 getContentType :: String -> String
 getContentType path 
-	| extension == ".html"        = "text/html"
-	| extension == ".txt"         = "text/plain"
-	| extension == ".css"         = "text/css"
-	| extension =~ "(.jpeg|.jpg)" = "image/jpeg"
-	| extension == ".png"         = "image/png"
-	| extension == ".gif"         = "image/gif"
-	| otherwise = "text/plain"
-	where extension = takeExtension path
+    | extension == ".html"        = "text/html"
+    | extension == ".txt"         = "text/plain"
+    | extension == ".css"         = "text/css"
+    | extension =~ "(.jpeg|.jpg)" = "image/jpeg"
+    | extension == ".png"         = "image/png"
+    | extension == ".gif"         = "image/gif"
+    | otherwise = "text/plain"
+    where extension = takeExtension path
 
 stripWWW :: String -> String
 stripWWW ('w':'w':'w':'.':xs) = xs
@@ -128,4 +128,4 @@ stripFS xs = xs
 
 getFileSize :: String -> IO Integer
 getFileSize path =
-	openFile path ReadMode >>= hFileSize 
+    openFile path ReadMode >>= hFileSize 
